2ef78b608768be83a9867097f03a5a8e240cdd47
MuhammadAmmar001 <muhammadammar001002@gmail.com>
2023-12-30 12:35:34 +0500

    changing username -> email in login and register api

--- src/UserAccMgmtServ/app/routes.py
+++ src/UserAccMgmtServ/app/routes.py
@@ -1,7 +1,6 @@
 # src/user_acc_mgmt_serv/routes.py
 from flask import request, jsonify
 from __init__ import app, user_collection
-from models import User
 
 
 @app.route("/")
@@ -18,7 +17,7 @@ def register_user():
         new_user = {
             'first_name': data.get('firstName', ''),
             'last_name': data.get('lastName', ''),
-            'username': data.get('email', ''),
+            'email': data.get('email', ''),
             'password': data.get('password', '')
         }
         user_collection.insert_one(new_user)
@@ -29,13 +28,18 @@ def register_user():
 
 
 # API to login (authentication)
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login_user():
     if request.method == 'POST':
         data = request.json
 
-        user = User.query.filter_by(
-            username=data['username'], password=data['password']).first()
+        email = data.get('email', '')
+        password = data.get('password', '')
+
+        user = user_collection.find_one(
+            {'email': email, 'password': password})
+        # user = User.query.filter_by(
+        #     username=data['username'], password=data['password']).first()
 
         if user:
             return jsonify({'message': 'Login successful'})
